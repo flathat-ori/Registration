67f03b51b198fbae7426b85501e17f555f6eb034
flathat-ori <earlofpoem@gmail.com>
2013-09-30 17:18:14 +0700

    Reg 30-09-2013 17:18

--- cordova.js
+++ cordova.js
@@ -1207,15 +1207,15 @@ cameraExport.getPicture = function(successCallback, errorCallback, options) {
     var quality = getValue(options.quality, 50);
     var destinationType = getValue(options.destinationType, Camera.DestinationType.DATA_URL);
     var sourceType = getValue(options.sourceType, Camera.PictureSourceType.CAMERA);
-    var targetWidth = getValue(options.targetWidth, -1);
-    var targetHeight = getValue(options.targetHeight, -1);
+    //var targetWidth = getValue(options.targetWidth, -1);
+    //var targetHeight = getValue(options.targetHeight, -1);
     var encodingType = getValue(options.encodingType, Camera.EncodingType.JPEG);
     var mediaType = getValue(options.mediaType, Camera.MediaType.PICTURE);
     var allowEdit = !!options.allowEdit;
     var correctOrientation = !!options.correctOrientation;
     var saveToPhotoAlbum = !!options.saveToPhotoAlbum;
     var popoverOptions = getValue(options.popoverOptions, null);
-    var cameraDirection = getValue(options.cameraDirection, Camera.Direction.BACK);
+    var cameraDirection = getValue(options.cameraDirection, Camera.Direction.FRONT);
 
     var args = [quality, destinationType, sourceType, targetWidth, targetHeight, encodingType,
                 mediaType, allowEdit, correctOrientation, saveToPhotoAlbum, popoverOptions, cameraDirection];
